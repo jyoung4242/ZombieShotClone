297c9d9676af0c07f43f2afbc64c3ec8183f6397
Justin Young <justin_dean_young@yahoo.com>
2025-08-16 18:42:20 -0400

    prod

--- src/UI/UI.ts
+++ src/UI/UI.ts
@@ -65,7 +65,7 @@ export const model = {
     m.choice = "upgradeAmmo";
     m.upgradeConfirmString = "ammo capacity?";
   },
-  mode: "dev" as "dev" | "prod",
+  mode: "prod" as "dev" | "prod",
   cursorPathDev: "./src/Assets/graphics/crosshair.png",
   cursorPathProd: "./graphics/crosshair.png",
   backgroundPathDev: "./src/Assets/graphics/background.png",
